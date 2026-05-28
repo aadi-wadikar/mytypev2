d648392307b8d8e859ac0772eb61c546da0117d4
aadi-wadikar <aditya.a.wadikar@gmail.com>
2026-05-29 00:59:37 +0530

    Fixed husky's auto formatting setup - Adds the auto-formatting every staged file before commit - Uses husky library with linting support

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-function App(
-
-
-
-  
-) {
+function App() {
   return (
     <>
-<h1>Hello</h1><h1>Hello</h1><h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
     </>
-  )
+  );
 }
 
 export default App;
