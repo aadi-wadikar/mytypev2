676b19740261363037446dea59ada7cf0b4dfd78
aadi-wadikar <aditya.a.wadikar@gmail.com>
2026-05-29 02:38:39 +0530

    Add the redux store setup - Added react-redux dependancy - Configured store in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import "./index.css";
 import "./styles/App.css";
 import "./styles/Common.css";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./store/store.tsx";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
